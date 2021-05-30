e5cb24b2e15f72c440f83319ab22e56abdcc5d2c
Leo <leo.lahti@iki.fi>
2021-05-30 17:58:25 +0300

    ok

--- code/joint_analysis.R
+++
@@ -1,7 +0,0 @@
-# Load data subsets
-source("data.R")
-
-# Analyse
-rmarkdown::render(input = "place.Rmd",    output_format = "md_document")
-rmarkdown::render(input = "language.Rmd", output_format = "md_document")
-
--- code/main.R
+++ code/main.R
@@ -1,1 +1,7 @@
-source("joint_analysis.R")
+# Load data subsets
+source("data.R")
+
+# Analyse
+rmarkdown::render(input = "place.Rmd",    output_format = "md_document")
+rmarkdown::render(input = "language.Rmd", output_format = "md_document")
+
