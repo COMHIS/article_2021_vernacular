62a2a43c654adc44548fb22a74a220ac83cc180a
Leo <leo.lahti@iki.fi>
2021-05-30 17:52:51 +0300

    ok

---
+++ code/data.R
@@ -0,0 +1,6 @@
+catalogs <- list()
+catalogs[["estc"]] <- readRDS("../estc/estc_subset.rds")
+catalogs[["snb"]] <- readRDS("../snb/snb_subset.rds")
+catalogs[["hpb"]] <- readRDS("../hpb/hpb_subset.rds")
+catalogs[["stcn"]] <- readRDS("../stcn/stcn_subset.rds")
+catalogs[["fnb"]] <- readRDS("../fnb/fnb_subset.rds")
