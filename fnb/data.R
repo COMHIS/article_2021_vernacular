68bf7df01082a48bdb266b9fedb7309b922ae82f
Leo <leo.lahti@iki.fi>
2021-06-05 13:51:38 +0300

    first analysis ready version for fnb

--- fnb/data.R
+++ fnb/data.R
@@ -1,9 +1,3 @@
 catalog <- "fnb";
 dat <- readRDS("../fnb/data/unified/polished/df.Rds")
 
-# FNB countries
-country <- c("Finland")
-subdat <- dat %>% filter(publication_country %in% country)
-# Save the selected subset
-saveRDS(subdat, file = "fnb_subset.rds")
-
--- fnb/main.R
+++ fnb/main.R
@@ -3,3 +3,17 @@ source("data.R")
 
 
 
+# FNB countries
+country <- c("Finland")
+subdat <- dat %>% filter(publication_country %in% country)
+
+fields <- c("original_row","publication_place","publication_country","multilingual","languages","language_primary","author_name","title","pagecount","gatherings","width","height","publisher","author","publication_year","publication_decade")
+subdat <- subdat[, fields]
+
+# Save the selected subset
+saveRDS(subdat, file = "fnb_subset.rds")
+
+
+
+
+
