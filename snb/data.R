0535fb95fee931e3baab40d3d7dbda722361c52f
Leo <leo.lahti@iki.fi>
2021-06-05 13:46:24 +0300

    first analysis ready version of snb

--- snb/data.R
+++ snb/data.R
@@ -1,9 +1,4 @@
 catalog <- "snb";
 dat <- readRDS("../snb/data/unified/polished/df.Rds")
 
-# SNB countries
-country <- c("Sweden")
-subdat <- dat %>% filter(publication_country %in% country)
-# Save the selected subset
-saveRDS(subdat, file = "snb_subset.rds")
 
--- snb/main.R
+++ snb/main.R
@@ -1,5 +1,13 @@
 source("init.R")
 source("data.R")
 
+# SNB countries
+country <- c("Sweden")
+subdat <- dat %>% filter(publication_country %in% country)
 
+fields <- c("original_row", "publication_country","publication_place","multilingual","languages","language_primary","author_name","title", "pagecount","publisher","author","publication_year","publication_decade","gatherings","width","height","singlevol", "multivol")
+subdat <- subdat[, fields]
+
+# Save the selected subset
+saveRDS(subdat, file = "snb_subset.rds")
 
