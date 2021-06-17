4b43815e0d7bea130a005ea454afb4eca9c74ce0
Leo <leo.lahti@iki.fi>
2021-06-17 14:54:49 +0300

    added stcn town

--- code/main.R
+++ code/main.R
@@ -4,6 +4,24 @@
 
 # Analyse
 catalogs <- readRDS("../output/dataset.Rds")
+
+# Selected towns
+selected.towns <- as.data.frame(rbind(
+  c("London", "estc"),
+  c("Oxford", "estc"),
+  c("Cambridge", "estc"),
+  c("Stockholm", "snb"),
+  c("Lund", "snb"),
+  c("Uppsala", "snb"),
+  c("Turku", "fnb"),
+  c("Leiden", "stcn"),
+  c("Amsterdam", "stcn"),
+  c("The Hague", "stcn"),
+  c("Utrecht", "stcn"),
+  c("Rotterdam", "stcn")))
+names(selected.towns) <- c("town", "catalog")
+
+
 rmarkdown::render(input = "place.Rmd",    output_format = "md_document")
 rmarkdown::render(input = "language.Rmd", output_format = "md_document")
 
