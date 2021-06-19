ec798f7a04e9f83e493964b51b15714e6e4ae621
Leo <leo.lahti@iki.fi>
2021-06-19 15:02:05 +0300

    lat and verna fields for fnb

--- code/fnb/main.R
+++ code/fnb/main.R
@@ -5,7 +5,11 @@ source("data.R")
 country <- c("Finland")
 subdat <- dat %>% filter(publication_country %in% country)
 
-fields <- c("original_row","publication_place","publication_country","multilingual","languages","language_primary","author_name","title","pagecount","gatherings","width","height","publisher","author","publication_year","publication_decade")
+fields <- c("original_row","publication_place","publication_country","multilingual",
+"language_primary","language_other","language_all",
+"language_latin_primary","language_latin_secondary",
+"language_vernacular_primary","language_vernacular_secondary",
+"author_name","title","pagecount","gatherings","width","height","publisher","author","publication_year","publication_decade")
 subdat <- subdat[, fields]
 
 # Save the selected subset
