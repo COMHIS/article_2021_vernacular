0f3ab975582401e6bb2b7128998df511252bbb44
Leo <leo.lahti@iki.fi>
2021-06-19 13:50:22 +0300

    snb latin and verna

--- code/snb/data.R
+++ code/snb/data.R
@@ -2,3 +2,7 @@ catalog <- "snb";
 dat <- readRDS("../snb/data/unified/polished/df.Rds")
 
 
+# dat <- dat %>% rename(language_all = languages)
+dat$languages <- NULL
+dat$language_latin_primary <-  grepl("Latin", dat$language_primary)
+dat$language_vernacular_primary <-  grepl("Swedish", dat$language_primary)
--- code/snb/main.R
+++ code/snb/main.R
@@ -7,11 +7,9 @@ subdat <- dat %>% filter(publication_country %in% country)
 
 fields <- c("original_row", "publication_country","publication_place","multilingual","language_primary","author_name","title", "pagecount","publisher","author","publication_year","publication_decade","gatherings","width","height","singlevol", "multivol")
 
+# No other lang info than primary, at least not fetched yet from the raw data
 # language == language_primary
-
 subdat <- subdat[, fields]
-
-
 
 # Save the selected subset
 saveRDS(subdat, file = "snb_subset.rds")
