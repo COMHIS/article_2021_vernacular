602a92f55607b7f16fe0ddbdfc362cefbac55906
Leo <leo.lahti@iki.fi>
2021-08-06 11:19:01 +0300

    snb lang fix

--- code/snb/main.R
+++ code/snb/main.R
@@ -3,14 +3,16 @@ source("data.R")
 
 # SNB countries
 country <- c("Sweden")
-subdat <- dat # %>% filter(publication_country %in% country)
+# subdat <- dat # %>% filter(publication_country %in% country)
 
 fields <- c("original_row", "publication_country","publication_place","multilingual","language_primary","author_name","title", "pagecount","publisher","author","publication_year","publication_decade","gatherings","width","height","singlevol", "multivol")
 
+# ------------------------------------------------------------------------
+
 # No other lang info than primary, at least not fetched yet from the raw data
 # language == language_primary
-subdat <- subdat[, fields]
+dat <- dat[, fields]
 
 # Save the selected subset
-saveRDS(subdat, file = "snb_subset.rds")
+saveRDS(dat, file = "snb_subset.rds")
 
