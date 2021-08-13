1ab83c6bf3a3c19e34a67e356e2869c7569c863b
Leo <leo.lahti@iki.fi>
2021-08-13 10:28:08 +0300

    estc title fixed

--- code/estc/main.R
+++ code/estc/main.R
@@ -1,8 +1,8 @@
 source("init.R")
 source("data.R")
 
-#places <- c("London", "Dublin", "Edinburgh", "Glasgow", "Oxford", "Cambridge", "Newcastle")
-#subdat <- dat %>% dplyr::filter(publication_place %in% places)
+# places <- c("London", "Dublin", "Edinburgh", "Glasgow", "Oxford", "Cambridge", "Newcastle")
+# subdat <- dat %>% dplyr::filter(publication_place %in% places)
 
 # Save the selected subset
 saveRDS(dat, file = "estc_subset.rds")
