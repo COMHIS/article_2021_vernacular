9f82246dee3316f42ba14395facd3d06c96c28d0
antagomir <leo.lahti@iki.fi>
2021-09-16 12:08:38 +0300

    Fig 1 with loess

--- code/work/leo/Figure1.R
+++ code/work/leo/Figure1.R
@@ -24,8 +24,12 @@ cols <- default_colors("catalog")[unique(toupper(dfs$catalog))]
 p <- ggplot(dfs, aes(x = publication_decade, y = f,
                      color=catalog, fill=catalog)) +
        #geom_bar(position="dodge", stat="identity") +
+       #geom_point() +
+       #geom_line() +
+       
        geom_point() +
-       geom_line() + 
+       geom_smooth(se=FALSE) +
+       
        theme_comhis("discrete", base_size=20) +
        labs(x = "Publication decade", y = "Latin share (%)") +
        scale_y_continuous(label=scales::percent, limits=c(0,1)) +
